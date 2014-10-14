51c26725fe5fa193923f8e5533e82311620865ab
George Wang <georgetw1108@gmail.com>
2014-10-14 11:50:31 -0700

    added eclipse .classpath and .project.

--- core/junittest/com/room8/flameengine/test/ScreenManagerTest.java
+++ core/junittest/com/room8/flameengine/test/ScreenManagerTest.java
@@ -40,19 +40,24 @@ public class ScreenManagerTest {
 	}
 	
 	@Test
-	public void testUpdate() {
-		fail("Not yet implemented");
+	public void testUpdateShouldBlock() {
+		m_screenManager.pushPreloadedScreen(m_gameplay1);
+		m_screenManager.pushPreloadedScreen(m_gameplay2);
+		when(m_gameplay2.blockUpdates()).thenReturn(true);
+		m_screenManager.update(0);
+		verify(m_gameplay2).update(0);
+		verify(m_gameplay1, never()).update(0);
 	}
 
 	@Test
-	public void testPop() {
+	public void testPopShouldRemoveScreen() {
 		m_screenManager.pushPreloadedScreen(m_gameplay1);
 		m_screenManager.pushPreloadedScreen(m_gameplay2);
 		m_screenManager.pop();
 		m_screenManager.update(0);
 		verify(m_gameplay2, never()).update(0);
 	}
-
+	
 	@Test
 	public void testPushPreloadedScreen() {
 		fail("Not yet implemented");
